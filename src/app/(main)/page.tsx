bafa0d3dbe91b57e5b46ed7b8face1bc78779b90
Omar Faruk <omarfaruk3393@gmail.com>
2025-10-16 19:05:29 +0600

    refactor(home): clean up layout and remove unused elements in Home component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,7 +6,6 @@ import CreatePostCard from '@/components/create-post-card';
 import FeedPostList from '@/components/post/feed-post-list';
 import SearchBar from '@/components/searchbar';
 import PostFilterSkeleton from '@/components/skeleton/post-filter-skeleton';
-import { Separator } from '@/components/ui/separator';
 import getLoginUser from '@/lib/get-login-user';
 
 export default async function Home() {
@@ -14,23 +13,14 @@ export default async function Home() {
   if (!user) return redirect('/login');
   return (
     <>
-      <div className="sticky top-0 bg-background z-50 p-4 lg:p-6 pb-0">
+      <div className="sticky top-0 bg-background z-50 p-4 lg:p-4 pb-0">
         <div className="">
-          <div className="lg:hidden mb-6">
+          <div className="lg:hidden mb-4 pb-2">
             <Suspense fallback={<PostFilterSkeleton />}>
               {/* <PostFilter /> */}
               <SearchBar />
             </Suspense>
           </div>
-
-          <h1 className="text-2xl font-semibold text-primary">
-            Gardening Tips & Advice
-          </h1>
-          <p className="my-3 text-muted-foreground">
-            Explore the latest gardening tips and advice shared by the
-            community.
-          </p>
-          <Separator className="mb-4" />
         </div>
       </div>
       <div className="p-4 lg:p-6">
