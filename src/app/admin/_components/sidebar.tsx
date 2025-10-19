02f87835f93d771d4bebe0b8627bd97e9c5334a0
Omar Faruk <omarfaruk3393@gmail.com>
2025-10-19 20:48:58 +0600

    refactor(sidebar): clean up and optimize Sidebar component structure

--- src/app/admin/_components/sidebar.tsx
+++ src/app/admin/_components/sidebar.tsx
@@ -4,8 +4,8 @@ import SidebarNav from './sidebar-nav';
 
 export default function Sidebar() {
   return (
-    <div className="hidden border-r bg-muted/40 md:block">
-      <div className="flex h-full max-h-screen flex-col gap-2 sticky top-0 left-0">
+    <div className="hidden  bg-background md:block">
+      <div className="flex h-full max-h-screen flex-col gap-2 fixed top-0 left-0 bg-background border-r">
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <DashboardLogo />
         </div>
